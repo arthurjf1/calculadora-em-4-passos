78f044633b33f8334291dafa3e9686ba26659aa8
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 20:16:30 -0300

    calcula soma e subtracao

--- calculadora.py
+++ calculadora.py
@@ -3,3 +3,5 @@ n1=int(input("digite o primeiro valor.....:"))
 n2=int(input("digite o segundo valor......:"))
 soma=n1+n2
 print("soma dos valores",soma)
+subtracao=n1-n2
+print("DIFERENÇA ENTRE OS VALORES",subtracao)
